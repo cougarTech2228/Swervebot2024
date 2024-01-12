1be2d0e9fe7b631dbb0d116853da50dc79e6d505
rtorpey7 <rtorpey7@gmail.com>
2024-01-11 19:20:16 -0500

    Initial 2024 commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,93 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.Orchestra;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.generated.TunerConstants;
+
+public class RobotContainer {
+  private double MaxSpeed = 2; // 6 meters per second desired top speed
+  private double MaxAngularRate = Math.PI; // Half a rotation per second max angular velocity
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
+
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+                                                               // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+  Orchestra orchestra = new Orchestra();
+  /* Path follower */
+  // Command runAuto = drivetrain.getAutoPath("Tests");
+
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  private void configureBindings() {
+    drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                           // negative Y (forward)
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        ).ignoringDisable(true));
+
+    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    joystick.b().whileTrue(drivetrain
+        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+
+    // reset the field-centric heading on left bumper press
+    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+    joystick.y().onTrue(new InstantCommand(new Runnable() {
+
+      @Override
+      public void run() {
+        System.out.println("Y button");
+        orchestra.play();
+      }
+      
+    }));
+    // if (Utils.isSimulation()) {
+    //   drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
+    // }
+    drivetrain.registerTelemetry(logger::telemeterize);
+
+    joystick.pov(0).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
+    joystick.pov(180).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
+  }
+
+  public RobotContainer() {
+    configureBindings();
+    
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(0).getDriveMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(0).getSteerMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(1).getDriveMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(1).getSteerMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(2).getDriveMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(2).getSteerMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(3).getDriveMotor());
+    orchestra.addInstrument(TunerConstants.DriveTrain.getModule(3).getSteerMotor());
+    orchestra.loadMusic("test.chrp");
+  }
+
+  public Command getAutonomousCommand() {
+    /* First put the drivetrain into auto run mode, then run the auto */
+    return Commands.runOnce(() -> {});
+  }
+}
